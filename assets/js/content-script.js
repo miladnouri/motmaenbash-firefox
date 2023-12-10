7b2ed4bf3db3057d80fdd42a92160bb52b6c9148
Milad <milad.nu@gmail.com>
2023-12-11 01:29:11 +0330

    Use Vazirmatn variable font Tiny UI improvement Remove cbinasim.ir payment gateway Add Readme file

--- assets/js/content-script.js
+++ assets/js/content-script.js
@@ -1,16 +1,7 @@
-var url = new URL(location.href);
-if( (url.hostname=='ipg.cbinasim.ir' || url.hostname.match(/\.shaparak\.ir$/i) ) && url.protocol == "https:" ){
-
-  var overlayDiv=document.createElement("div");
+let url = new URL(window.location.href);
+if (url.hostname.match(/\.shaparak\.ir$/i) && url.protocol == "https:") {
+  let overlayDiv = document.createElement("div");
   overlayDiv.id = "motmaenBashCornerSign";
-  overlayDiv.innerHTML = '<a id="motmaenBashCornerSignLink" href="https://tookan.tech/?utm_source=firefox&utm_medium=motmaenbash&utm_campaign=extensions&utm_term=sign_logo" target="_blank"></a>'
+  overlayDiv.innerHTML = '<a href="https://motmaenbash.milad.nu" target="_blank"><img title="این درگاه پرداخت معتبر و امن است" id="motmaenBashCornerSignLogo" src="' + browser.runtime.getURL('assets/images/sign.png') + '"/></a>';
   document.body.insertBefore(overlayDiv, document.body.firstChild);
-
-  var motmaenBashLogoImg = document.createElement("img");
-  motmaenBashLogoImg.setAttribute("src", chrome.runtime.getURL('assets/images/sign.png'));
-  motmaenBashLogoImg.setAttribute("title", "این درگاه پرداخت معتبر و امن است");
-  motmaenBashLogoImg.setAttribute("target", "_blank");
-  motmaenBashLogoImg.id = "motmaenBashCornerSignLogo";
-  document.getElementById('motmaenBashCornerSignLink').appendChild(motmaenBashLogoImg);
-
 }
