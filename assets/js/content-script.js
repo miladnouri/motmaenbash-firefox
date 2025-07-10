9d27e8156ceb902757041c653f4d8129a4f0b63b
pouriiia <cloner.1383@gmail.com>
2025-07-10 19:18:07 +0330

    fix & add link

--- assets/js/content-script.js
+++ assets/js/content-script.js
@@ -2,6 +2,6 @@ let url = new URL(window.location.href);
 if (url.hostname.match(/\.shaparak\.ir$/i) && url.protocol == "https:") {
   let overlayDiv = document.createElement("div");
   overlayDiv.id = "motmaenBashCornerSign";
-  overlayDiv.innerHTML = '<a href="https://motmaenbash.milad.nu" target="_blank"><img title="این درگاه پرداخت معتبر و امن است" id="motmaenBashCornerSignLogo" src="' + browser.runtime.getURL('assets/images/sign.png') + '"/></a>';
+  overlayDiv.innerHTML = '<a href="https://motmaenbash.ir/" target="_blank"><img title="این درگاه پرداخت معتبر و امن است" id="motmaenBashCornerSignLogo" src="' + browser.runtime.getURL('assets/images/sign.png') + '"/></a>';
   document.body.insertBefore(overlayDiv, document.body.firstChild);
 }
